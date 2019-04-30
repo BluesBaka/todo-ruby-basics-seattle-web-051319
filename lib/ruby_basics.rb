8004564e64b19fe692490dec62be701fc5c37573
Bryna Dunnells <badunnells@gmail.com>
2019-04-30 15:31:26 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+    return "#{num1}/#{num2}"
 end
 
 def assign_variable(value)
-  
+  var = #{value}
 end
 
-def argue
-  
-end
-
-def greeting
+def argue()
 
 end
 
-def return_a_value
+def greeting(greeting, name)
 
+end
+
+def return_a_value(value)
+    return "#{value}"
 end
 
 def last_evaluated_value
 
 end
 
-def pizza_party
-
-end
+##def pizza_party
+#    return "cheese"
+#end
