d5d1b7c998e5f109430c7b6a1b3411c453b22702
Bryna Dunnells <badunnells@gmail.com>
2019-04-30 15:44:20 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,9 +2,9 @@ def division(num1, num2)
     return "#{num1}/#{num2}"
 end
 
-def assign_variable(value)
-  var = #{value}
-end
+#def assign_variable(value)
+#  var = #{value}
+#end
 
 def argue(phrase)
     return "#{phrase}"
